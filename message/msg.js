85a247fc4ce2d6a5605088d4287280064cd3fea0
Zaki1726 <100398414+Zaki1726@users.noreply.github.com>
2022-08-12 19:45:44 +0700

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -808,7 +808,32 @@ limitAdd(sender, limit)
                     reply('Sukses!')
                 }
                 break
-             // Stalk Menu
+case 'rules'
+
+var rul =`◈───〖 *RULES* 〗───◈
+
+1. Tolong Gunakan Delay 5 Detik Untuk Menggukan BOT
+2. Gunakan Dengan Bijak.
+3. Don't Call Bot 
+4. Don't Spam Bot
+
+◈───〖 *SNK* 〗───◈
+1. Data Whatsapp Anda Akan Kami Simpan Di Server Kami Selama BOT Aktif.
+2. Data Anda Akan Di Hapus Ketika BOT OFFLINE
+3. Kami Tidak Menyimpan Gambar, Video, File, Audio, Dan Dokumen Yang Anda Kirim
+4. Kami Tidak Akan Pernah Meminta Anda Untuk Memberikan Informasi Pribadi
+5. Jika Menemukan BUG/ERROR Silahkan Langsung Lapor Ke OWNER BOT!
+
+
+◈───〖 *ATTENTION* 〗───◈
+*KONSEKUENSI BILA MELANGGAR RULES*
+- Bot Akan Memblokir Nomor Anda.
+- Anda Tidak Akan Bisa Mengakses Bot Lagi.
+- Mengeksploitasi Terhadap bot.
+Sanksi: *PERMANENT BLOCK*`
+
+conn.sendMessage(from, { caption: rul, image: pp_bot }, {quoted: fdoc})
+break             // Stalk Menu
             case prefix+'ghstalk':
   case prefix+'stalkgh':
     if (isLimit(sender, isPremium, isOwner, limitCount, limit)) return reply (`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
